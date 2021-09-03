3c8215eab793bf68b993958aeab80571b5d2ad36
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2021-09-02 17:50:36 -0700

    ENH: Add an interface to create manifest files

---
+++ metapool/igm.py
@@ -0,0 +1,116 @@
+import os
+
+from datetime import datetime
+
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+
+_IGM_ROW_OFFSET = 25
+
+
+def _property_maker(obj, name, cell, default):
+    def fget(self):
+        return getattr(self, '_' + name)
+
+    def fset(self, value):
+        # print('what')
+        if value is not None:
+            self._sheet[cell] = value
+        setattr(self, '_' + name, value)
+
+    # set the default values
+    setattr(obj, '_' + name, default)
+    if default is not None:
+        getattr(obj, '_sheet')[cell] = default
+
+    # print('i did something')
+    # setattr(obj, name, default)
+    # get class of self and assign property
+
+    setattr(type(obj), name, property(fget, fset))
+
+
+class IGMManifest(object):
+    def __init__(self):
+        self._workbook = _load_igm_template()
+        self._sheet = self._workbook['Sample Information']
+
+        _property_maker(self, 'submission_date', 'B2',
+                        datetime.strftime(datetime.today(), '%m/%d/%y'))
+        _property_maker(self, 'institute', 'B3', 'Knight Lab')
+        _property_maker(self, 'pi_name', 'B4', 'Dr. Knight')
+        _property_maker(self, 'pi_email', 'B5', 'mackenzie.m.bryant@gmail.com')
+        _property_maker(self, 'contact_name', 'B6', 'MacKenzie Bryant')
+        _property_maker(self, 'contact_email', 'B7',
+                        'mackenzie.m.bryant@gmail.com')
+
+        _property_maker(self, 'project_number', 'B12', None)
+        _property_maker(self, 'task_number', 'B13', None)
+
+        _property_maker(self, 'platform', 'B18', 'NovaSeq S4')
+        _property_maker(self, 'run_type', 'B19', 'PE150')
+        _property_maker(self, 'custom_primer', 'B20',
+                        'No-Standard Illumina Primers are fine')
+
+        _property_maker(self, 'number_of_samples', 'B22', None)
+        _property_maker(self, 'number_of_lanes', 'B23', '1')
+
+        self._pools = None
+
+        # default value requested by IGM
+        self._sheet['D1'] = '150x8x8x150'
+        self._sheet['D1'].fill = PatternFill(fill_type='solid',
+                                             fgColor='FFFF00')
+
+    @property
+    def pools(self):
+        return self._pools
+
+    @pools.setter
+    def pools(self, value):
+        """List of pools to write in the manifest"""
+        if value is None:
+            for i, pool in enumerate(self._pools):
+                row = str(_IGM_ROW_OFFSET + i)
+
+                del self._sheet['A' + row]
+                del self._sheet['B' + row]
+                del self._sheet['C' + row]
+                del self._sheet['D' + row]
+        else:
+            for i, pool in enumerate(value):
+                row = str(_IGM_ROW_OFFSET + i)
+
+                self._sheet['A' + row] = pool
+                self._sheet['B' + row] = pool
+                self._sheet['C' + row] = 500
+                self._sheet['D' + row] = 'KHP'
+
+        self._pools = value
+
+    def write(self, path=None):
+        required = ['project_number', 'task_number', 'platform',
+                    'number_of_samples', 'number_of_lanes', 'pools']
+        for prop in required:
+            if getattr(self, prop) is None:
+                raise ValueError('%s cannot be empty, you need to set a value'
+                                 % prop)
+
+        if path is None:
+            path = (datetime.strftime(datetime.today(), '%Y_%m_%d_') +
+                    # should be the last name
+                    self.pi_name.split(' ')[-1] + '_' +
+                    # all the pools with spaces replaced for underscores
+                    '_'.join([p.replace(' ', '_') for p in self._pools]) +
+                    '_Manifest_2021.xlsx')
+        print('Saving manifest to %s' % path)
+
+        self._workbook.save(path)
+
+
+def _load_igm_template():
+    """Helper function to load IGM's manifest template"""
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data',
+                        'template.xlsx')
+    # print(path)
+    return load_workbook(path)
---
+++ metapool/tests/test_igm.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class IGMManifestTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_raises_error_with_missing_properties(self):
+        self.fail()
+
+    def test_raises_error_with_missing_properties_single(self):
+        self.fail()
+
+    def test_default_values(self):
+        self.fail()
+
+    def test_set_some_pools(self):
+        self.fail()
+
+    def test_delete_pools(self):
+        self.fail()
+
+    def test_write_and_load(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
