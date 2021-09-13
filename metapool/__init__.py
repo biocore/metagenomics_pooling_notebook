6482d0049902c2f7203e1c647c6a4beca35a56f3
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2021-09-13 09:21:07 -0700

    ENH: Add versioneer to project (#17)
    * ENH: Add versioneer to project
    
    * STY: break long line

--- metapool/__init__.py
+++ metapool/__init__.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
-from metapool.prep import preparations_for_run, parse_prep
-from metapool.sample_sheet import (sample_sheet_to_dataframe, KLSampleSheet,
-                                   validate_sample_sheet, make_sample_sheet)
-from metapool.plate import validate_plate_metadata
-from metapool.amplipool import assign_emp_index
-from metapool.igm import IGMManifest
+from .prep import preparations_for_run, parse_prep
+from .sample_sheet import (sample_sheet_to_dataframe, KLSampleSheet,
+                           validate_sample_sheet, make_sample_sheet)
+from .plate import validate_plate_metadata
+from .amplipool import assign_emp_index
+from .igm import IGMManifest
 
-__credits__ = ("https://github.com/tanaes/metagenomics_pooling_notebook/"
+__credits__ = ("https://github.com/biocore/metagenomics_pooling_notebook/"
                "graphs/contributors")
-__version__ = "0.0.0.dev0"
 __all__ = ['preparations_for_run', 'parse_prep',
            'sample_sheet_to_dataframe', 'KLSampleSheet',
            'validate_sample_sheet', 'make_sample_sheet',
@@ -17,3 +16,6 @@ __all__ = ['preparations_for_run', 'parse_prep',
            'validate_plate_metadata',
            'assign_emp_index',
            'IGMManifest']
+
+from . import _version
+__version__ = _version.get_versions()['version']
