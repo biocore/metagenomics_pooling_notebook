4b83a894a1579b72488a9614a35d3b69e5630d0e
Justin Shaffer <justinparkshaffer@gmail.com>
2023-03-27 20:22:00 -0700

    Update rescale_counts.py
    Removed function to generate read count output; updated function for generating cell counts to generate cell counts per gram of input sample material

--- metapool/rescale_counts.py
+++ metapool/rescale_counts.py
@@ -35,25 +35,18 @@ def _apply_model(df_log10_cpm, linear_models):
     return converted_reads, failed_colnames
 
 
-def convert_read_count_to_cell_count(df, metadata_features):
+def convert_read_count_to_cell_count_per_g_input(df, metadata_features, prep_info_samples):
     lengths = metadata_features.loc[df.index, "total_length"]
+    sample_weights = prep_info_samples.loc[df.index, "calc_mass_sample_aliquot_input_g"]
     # Careful with types here.  If you use ints,
     # the length * 650 * 10**9 can overflow integers with very long genomes
-    mult_row = (6.022 * (10.0 ** 23)) / (lengths * (650 * 10.0 ** 9))
+    mult_row = (6.022 * (10.0 ** 23)) / (lengths * (650 * 10.0 ** 9) * sample_weights)
     df = df.mul(mult_row, axis=0)
     return df
-
-
-def to_absolute_abundance_read_count(df, linear_models):
-    df = _to_log10_cpm(df)
-    df, failed_cols = _apply_model(df, linear_models)
-    df.index.name = "sample_name"
-    return df, failed_cols
-
 
 def to_absolute_abundance_cell_count(df, linear_models, metadata_features):
     df = _to_log10_cpm(df)
     df, failed_cols = _apply_model(df, linear_models)
-    df = convert_read_count_to_cell_count(df, metadata_features)
+    df = convert_read_count_to_cell_count_per_g_input(df, metadata_features, prep_info_samples)
     df.index.name = "sample_name"
     return df, failed_cols
