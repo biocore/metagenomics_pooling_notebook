1a8c887308d29894e5e27365d4df38466fb76d75
RodolfoSalido <rodolfo.salido@gmail.com>
2023-08-29 12:38:35 -0700

    moved add_syndna() to metapool.py

--- metapool/metapool.py
+++ metapool/metapool.py
@@ -1416,3 +1416,41 @@ def estimate_read_depth(plate_df,
     plt.show()
 
     return plate_df
+
+
+def add_syndna(plate_df, syndna_pool_number=None, syndna_concentration=None,
+               syndna_percentage=5):
+    """Calculates nanoliters of synDNA spike-in to add to each sample to achieve desired
+    sequencing percentage (% of reads per sample). 
+
+    Parameters
+    ----------
+    plate_df : pd.DataFrame
+        Growing plate dataframe with calculated gDNA input normalization values
+    syndna_pool_number : str (Default:None)
+        String formatted name for synDNA pool. Typically a number.
+    syndna_concentration : float (Default:None)
+        Concentration in ng/µL of synDNA spike-in
+    syndna_percentage : float (Default:5%)
+        Percentage of input in sample allocated for synDNA spike-in.
+    syndna_well: str (Default: A1)
+        Well location of synDNA spike-in stock in echo compatible plate. 
+
+    Returns
+    -------
+    plate_df : pd.DataFrame
+        returns a pandas dataframe with extra columns"""
+    
+    plate_df_ = plate_df.copy()
+    plate_df_['synDNA pool number'] = str(syndna_pool_number)
+    if syndna_pool_number==None:
+        print('Returning input plate dataframe; no synDNA will be added to this prep')
+        return(plate_df_)
+    else:
+        if 'Normalized DNA volume' not in plate_df_.columns:
+            raise Exception("The plate dataframe (plate_df) must have input normalization "+
+                        "values already calculated before calculating synDNA addition")
+        plate_df_['Input DNA'] = plate_df_['Sample DNA Concentration']*plate_df_['Normalized DNA volume']/1000
+        #synDNA volume is in nL
+        plate_df_['synDNA volume'] = 1000*(plate_df_['Input DNA'] * (syndna_percentage*10**-2)/syndna_concentration)
+        return(plate_df_)
