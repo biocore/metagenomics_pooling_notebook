86e35ff6367fda5b220837a7530b40885d303384
Charles Cowart <ccowart@ucsd.edu>
2023-08-28 18:33:29 -0700

    Replaced pool_N with poolN

--- metapool/tests/test_sample_sheet.py
+++ metapool/tests/test_sample_sheet.py
@@ -195,28 +195,28 @@ class KLSampleSheetTests(BaseTests):
 
         data = (
             '1,sample_1,sample.1,FooBar_666_p1,A1,iTru7_107_07,CCGACTAT,'
-            'iTru5_01_A,ACCGACAA,Baz,pool_1,importantsample1,'
+            'iTru5_01_A,ACCGACAA,Baz,pool1,importantsample1,'
             'KnightLabKapaHP,Eqiiperiment\n'
             '1,sample_2,sample.2,FooBar_666_p1,A2,iTru7_107_08,CCGACTAT,'
-            'iTru5_01_A,CTTCGCAA,Baz,pool_1,importantsample2,'
+            'iTru5_01_A,CTTCGCAA,Baz,pool1,importantsample2,'
             'KnightLabKapaHP,Eqiiperiment\n'
             '3,sample_1,sample.1,FooBar_666_p1,A3,iTru7_107_09,GCCTTGTT,'
-            'iTru5_01_A,AACACCAC,Baz,pool_1,importantsample1,'
+            'iTru5_01_A,AACACCAC,Baz,pool1,importantsample1,'
             'KnightLabKapaHP,Eqiiperiment\n'
             '3,sample_2,sample.2,FooBar_666_p1,A4,iTru7_107_10,AACTTGCC,'
-            'iTru5_01_A,CGTATCTC,Baz,pool_1,importantsample2,'
+            'iTru5_01_A,CGTATCTC,Baz,pool1,importantsample2,'
             'KnightLabKapaHP,Eqiiperiment\n'
             '3,sample_31,sample.31,FooBar_666_p1,A5,iTru7_107_11,CAATGTGG,'
-            'iTru5_01_A,GGTACGAA,FooBar_666,pool_1,importantsample31,'
+            'iTru5_01_A,GGTACGAA,FooBar_666,pool1,importantsample31,'
             'KnightLabKapaHP,SomethingWitty\n'
             '3,sample_32,sample.32,FooBar_666_p1,B6,iTru7_107_12,AAGGCTGA,'
-            'iTru5_01_A,CGATCGAT,FooBar_666,pool_1,importantsample32,'
+            'iTru5_01_A,CGATCGAT,FooBar_666,pool1,importantsample32,'
             'KnightLabKapaHP,SomethingWitty\n'
             '3,sample_34,sample.34,FooBar_666_p1,B8,iTru7_107_13,TTACCGAG,'
-            'iTru5_01_A,AAGACACC,FooBar_666,pool_1,importantsample34,'
+            'iTru5_01_A,AAGACACC,FooBar_666,pool1,importantsample34,'
             'KnightLabKapaHP,SomethingWitty\n'
             '3,sample_44,sample.44,Baz_p3,B99,iTru7_107_14,GTCCTAAG,'
-            'iTru5_01_A,CATCTGCT,Baz,pool_1,importantsample44,'
+            'iTru5_01_A,CATCTGCT,Baz,pool1,importantsample44,'
             'KnightLabKapaHP,Eqiiperiment\n'
         )
         keys = ['Lane', 'Sample_ID', 'Sample_Name', 'Sample_Plate',
@@ -579,7 +579,7 @@ class SampleSheetWorkflow(BaseTests):
              '515rcbc0', 'AATGATACGGCGACCACCGAGATCTACACGCT', 'AGCCTTCGTCGC',
              'TATGGTAATT', 'GT', 'GTGYCAGCMGCCGCGGTAA',
              'AATGATACGGCGACCACCGAGATCTACACGCTAGCCTTCGTCGCTATGGTAATTGTGTGYCAG'
-             'CMGCCGCGGTAA', 'pool_1'],
+             'CMGCCGCGGTAA', 'pool1'],
             ['X00180199',
              'X00180199', 'C', 1, False, 'THDMI_10317_PUK2', 'THDMI_10317',
              'THDMI_10317_UK2-US6', 'C1', '1', '1', 'SF', '166032128',
@@ -588,7 +588,7 @@ class SampleSheetWorkflow(BaseTests):
              '515rcbc12', 'AATGATACGGCGACCACCGAGATCTACACGCT', 'CGTATAAATGCG',
              'TATGGTAATT', 'GT', 'GTGYCAGCMGCCGCGGTAA',
              'AATGATACGGCGACCACCGAGATCTACACGCTCGTATAAATGCGTATGGTAATTGTGTGYCAG'
-             'CMGCCGCGGTAA', 'pool_1'],
+             'CMGCCGCGGTAA', 'pool1'],
             ['X00179789',
              'X00179789', 'E', 1, False, 'THDMI_10317_PUK2', 'THDMI_10317',
              'THDMI_10317_UK2-US6', 'E1', '1', '1', 'SF', '166032128',
@@ -597,7 +597,7 @@ class SampleSheetWorkflow(BaseTests):
              '515rcbc24', 'AATGATACGGCGACCACCGAGATCTACACGCT', 'TGACTAATGGCC',
              'TATGGTAATT', 'GT', 'GTGYCAGCMGCCGCGGTAA',
              'AATGATACGGCGACCACCGAGATCTACACGCTTGACTAATGGCCTATGGTAATTGTGTGYCAG'
-             'CMGCCGCGGTAA', 'pool_1'],
+             'CMGCCGCGGTAA', 'pool1'],
         ]
 
         self.table = pd.DataFrame(
@@ -685,22 +685,22 @@ class SampleSheetWorkflow(BaseTests):
         data = (
             [5, 'X00180471', 'X00180471', 'THDMI_10317_PUK2', 'A1', '515rcbc0',
              'AGCCTTCGTCGC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180471.A1'],
+             'pool1', 'THDMI_10317_PUK2.X00180471.A1'],
             [5, 'X00180199', 'X00180199', 'THDMI_10317_PUK2', 'C1',
              '515rcbc12', 'CGTATAAATGCG', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180199.C1'],
+             'pool1', 'THDMI_10317_PUK2.X00180199.C1'],
             [5, 'X00179789', 'X00179789', 'THDMI_10317_PUK2', 'E1',
              '515rcbc24', 'TGACTAATGGCC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00179789.E1'],
+             'pool1', 'THDMI_10317_PUK2.X00179789.E1'],
             [7, 'X00180471', 'X00180471', 'THDMI_10317_PUK2', 'A1', '515rcbc0',
              'AGCCTTCGTCGC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180471.A1'],
+             'pool1', 'THDMI_10317_PUK2.X00180471.A1'],
             [7, 'X00180199', 'X00180199', 'THDMI_10317_PUK2', 'C1',
              '515rcbc12', 'CGTATAAATGCG', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180199.C1'],
+             'pool1', 'THDMI_10317_PUK2.X00180199.C1'],
             [7, 'X00179789', 'X00179789', 'THDMI_10317_PUK2', 'E1',
              '515rcbc24', 'TGACTAATGGCC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00179789.E1'],
+             'pool1', 'THDMI_10317_PUK2.X00179789.E1'],
         )
         keys = ['Lane', 'Sample_ID', 'Sample_Name', 'Sample_Plate',
                 'well_id_384', 'I7_Index_ID', 'index', 'I5_Index_ID', 'index2',
@@ -725,22 +725,22 @@ class SampleSheetWorkflow(BaseTests):
         data = (
             [5, 'X00180471', 'X00180471', 'THDMI_10317_PUK2', 'A1', '515rcbc0',
              'AGCCTTCGTCGC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180471.A1'],
+             'pool1', 'THDMI_10317_PUK2.X00180471.A1'],
             [5, 'X00180199', 'X00180199', 'THDMI_10317_PUK2', 'C1',
              '515rcbc12', 'CGTATAAATGCG', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180199.C1'],
+             'pool1', 'THDMI_10317_PUK2.X00180199.C1'],
             [5, 'X00179789', 'X00179789', 'THDMI_10317_PUK2', 'E1',
              '515rcbc24', 'TGACTAATGGCC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00179789.E1'],
+             'pool1', 'THDMI_10317_PUK2.X00179789.E1'],
             [7, 'X00180471', 'X00180471', 'THDMI_10317_PUK2', 'A1', '515rcbc0',
              'AGCCTTCGTCGC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180471.A1'],
+             'pool1', 'THDMI_10317_PUK2.X00180471.A1'],
             [7, 'X00180199', 'X00180199', 'THDMI_10317_PUK2', 'C1',
              '515rcbc12', 'CGTATAAATGCG', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180199.C1'],
+             'pool1', 'THDMI_10317_PUK2.X00180199.C1'],
             [7, 'X00179789', 'X00179789', 'THDMI_10317_PUK2', 'E1',
              '515rcbc24', 'TGACTAATGGCC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00179789.E1'],
+             'pool1', 'THDMI_10317_PUK2.X00179789.E1'],
         )
         keys = ['Lane', 'Sample_ID', 'Sample_Name', 'Sample_Plate',
                 'well_id_384', 'I7_Index_ID', 'index', 'I5_Index_ID', 'index2',
@@ -787,13 +787,13 @@ class SampleSheetWorkflow(BaseTests):
         data = [
             ['X00180471', 'X00180471', 'THDMI_10317_PUK2', 'A1', '515rcbc0',
              'AGCCTTCGTCGC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180471.A1'],
+             'pool1', 'THDMI_10317_PUK2.X00180471.A1'],
             ['X00180199', 'X00180199', 'THDMI_10317_PUK2', 'C1', '515rcbc12',
              'CGTATAAATGCG', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180199.C1'],
+             'pool1', 'THDMI_10317_PUK2.X00180199.C1'],
             ['X00179789', 'X00179789', 'THDMI_10317_PUK2', 'E1', '515rcbc24',
              'TGACTAATGGCC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00179789.E1'],
+             'pool1', 'THDMI_10317_PUK2.X00179789.E1'],
         ]
 
         exp = pd.DataFrame(columns=columns, data=data)
@@ -813,16 +813,16 @@ class SampleSheetWorkflow(BaseTests):
         data = [
             ['33-A1', 'A', 1, True, 'A1', 0, 0, 'AACGCACACTCGTCTT',
              'iTru5_19_A', 'AACGCACA', 'A1', 'iTru5_plate', 'iTru7_109_01',
-             'CTCGTCTT', 'A22', 'iTru7_plate', '33-A1', 'pool_1',
+             'CTCGTCTT', 'A22', 'iTru7_plate', '33-A1', 'pool1',
              'The_plate.33-A1.A1'],
             ['820072905-2', 'C', 1, False, 'C1', 1, 1, 'ATGCCTAGCGAACTGT',
              'iTru5_19_B', 'ATGCCTAG', 'B1', 'iTru5_plate', 'iTru7_109_02',
              'CGAACTGT', 'B22', 'iTru7_plate', '820072905-2',
-             'pool_1', 'The_plate.820072905-2.C1'],
+             'pool1', 'The_plate.820072905-2.C1'],
             ['820029517-3', 'E', 1, False, 'E1', 2, 2, 'CATACGGACATTCGGT',
              'iTru5_19_C', 'CATACGGA', 'C1', 'iTru5_plate', 'iTru7_109_03',
              'CATTCGGT', 'C22', 'iTru7_plate', '820029517-3',
-             'pool_1', 'The_plate.820029517-3.E1']
+             'pool1', 'The_plate.820029517-3.E1']
         ]
         columns = ['Sample', 'Row', 'Col', 'Blank', 'Well', 'index',
                    'index combo', 'index combo seq', 'i5 name', 'i5 sequence',
@@ -839,13 +839,13 @@ class SampleSheetWorkflow(BaseTests):
         data = [
             ['33-A1', '33-A1', 'The_plate', 'A1', 'iTru7_109_01',
              'CTCGTCTT', 'iTru5_19_A', 'AACGCACA', 'Tst_project_1234',
-             'pool_1', 'The_plate.33-A1.A1'],
+             'pool1', 'The_plate.33-A1.A1'],
             ['820072905-2', '820072905-2', 'The_plate', 'C1', 'iTru7_109_02',
              'CGAACTGT', 'iTru5_19_B', 'ATGCCTAG', 'Tst_project_1234',
-             'pool_1', 'The_plate.820072905-2.C1'],
+             'pool1', 'The_plate.820072905-2.C1'],
             ['820029517-3', '820029517-3', 'The_plate', 'E1', 'iTru7_109_03',
              'CATTCGGT', 'iTru5_19_C', 'CATACGGA', 'Tst_project_1234',
-             'pool_1', 'The_plate.820029517-3.E1'],
+             'pool1', 'The_plate.820029517-3.E1'],
         ]
 
         exp = pd.DataFrame(columns=columns, data=data)
@@ -865,15 +865,15 @@ class SampleSheetWorkflow(BaseTests):
             ['33-A1', 'A', 1, True, 'A1', 0, 0, 'AACGCACACTCGTCTT',
              'iTru5_19_A', 'AACGCACA', 'A1', 'iTru5_plate', 'iTru7_109_01',
              'CTCGTCTT', 'A22', 'iTru7_plate', '33-A1',
-             'pool_1', 'The_plate.33-A1.A1'],
+             'pool1', 'The_plate.33-A1.A1'],
             ['820072905-2', 'C', 1, False, 'C1', 1, 1, 'ATGCCTAGCGAACTGT',
              'iTru5_19_B', 'ATGCCTAG', 'B1', 'iTru5_plate', 'iTru7_109_02',
              'CGAACTGT', 'B22', 'iTru7_plate', '820072905-2',
-             'pool_1', 'The_plate.820072905-2.C1'],
+             'pool1', 'The_plate.820072905-2.C1'],
             ['820029517-3', 'E', 1, False, 'E1', 2, 2, 'CATACGGACATTCGGT',
              'iTru5_19_C', 'CATACGGA', 'C1', 'iTru5_plate', 'iTru7_109_03',
              'CATTCGGT', 'C22', 'iTru7_plate', '820029517-3',
-             'pool_1', 'The_plate.820029517-3.E1']
+             'pool1', 'The_plate.820029517-3.E1']
         ]
         columns = ['Sample', 'Row', 'Col', 'Blank', 'Well', 'index',
                    'index combo', 'index combo seq', 'i5 name', 'i5 sequence',
@@ -890,13 +890,13 @@ class SampleSheetWorkflow(BaseTests):
         data = [
             ['33-A1', '33-A1', 'The_plate', 'A1', 'iTru7_109_01',
              'CTCGTCTT', 'iTru5_19_A', 'AACGCACA', 'Tst_project_1234',
-             'pool_1', 'The_plate.33-A1.A1'],
+             'pool1', 'The_plate.33-A1.A1'],
             ['820072905-2', '820072905-2', 'The_plate', 'C1', 'iTru7_109_02',
              'CGAACTGT', 'iTru5_19_B', 'ATGCCTAG', 'Tst_project_1234',
-             'pool_1', 'The_plate.820072905-2.C1'],
+             'pool1', 'The_plate.820072905-2.C1'],
             ['820029517-3', '820029517-3', 'The_plate', 'E1', 'iTru7_109_03',
              'CATTCGGT', 'iTru5_19_C', 'CATACGGA', 'Tst_project_1234',
-             'pool_1', 'The_plate.820029517-3.E1'],
+             'pool1', 'The_plate.820029517-3.E1'],
         ]
 
         exp = pd.DataFrame(columns=columns, data=data)
@@ -924,13 +924,13 @@ class SampleSheetWorkflow(BaseTests):
         data = (
             [1, 'X00180471', 'X00180471', 'THDMI_10317_PUK2', 'A1', '515rcbc0',
              'AGCCTTCGTCGC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180471.A1'],
+             'pool1', 'THDMI_10317_PUK2.X00180471.A1'],
             [1, 'X00180199', 'X00180199', 'THDMI_10317_PUK2', 'C1',
              '515rcbc12', 'CGTATAAATGCG', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00180199.C1'],
+             'pool1', 'THDMI_10317_PUK2.X00180199.C1'],
             [1, 'X00179789', 'X00179789', 'THDMI_10317_PUK2', 'E1',
              '515rcbc24', 'TGACTAATGGCC', '', '', 'THDMI_10317',
-             'pool_1', 'THDMI_10317_PUK2.X00179789.E1'],
+             'pool1', 'THDMI_10317_PUK2.X00179789.E1'],
         )
         keys = ['Lane', 'Sample_ID', 'Sample_Name', 'Sample_Plate',
                 'well_id_384', 'I7_Index_ID', 'index', 'I5_Index_ID', 'index2',
@@ -1388,28 +1388,28 @@ class DemuxReplicatesTests(BaseTests):
 
 DF_DATA = [
     ['1', 'sample.1', 'FooBar_666_p1', 'A1', 'iTru7_107_07', 'CCGACTAT',
-     'iTru5_01_A', 'ACCGACAA', 'Baz', 'pool_1', 'importantsample1',
+     'iTru5_01_A', 'ACCGACAA', 'Baz', 'pool1', 'importantsample1',
      'Knight Lab Kapa HP', 'Eqiiperiment'],
     ['1', 'sample.2', 'FooBar_666_p1', 'A2', 'iTru7_107_08', 'CCGACTAT',
-     'iTru5_01_A', 'CTTCGCAA', 'Baz', 'pool_1', 'importantsample2',
+     'iTru5_01_A', 'CTTCGCAA', 'Baz', 'pool1', 'importantsample2',
      'Knight Lab Kapa HP', 'Eqiiperiment'],
     ['3', 'sample.1', 'FooBar_666_p1', 'A3', 'iTru7_107_09', 'GCCTTGTT',
-     'iTru5_01_A', 'AACACCAC', 'Baz', 'pool_1', 'importantsample1',
+     'iTru5_01_A', 'AACACCAC', 'Baz', 'pool1', 'importantsample1',
      'Knight Lab Kapa HP', 'Eqiiperiment'],
     ['3', 'sample.2', 'FooBar_666_p1', 'A4', 'iTru7_107_10', 'AACTTGCC',
-     'iTru5_01_A', 'CGTATCTC', 'Baz', 'pool_1', 'importantsample2',
+     'iTru5_01_A', 'CGTATCTC', 'Baz', 'pool1', 'importantsample2',
      'Knight Lab Kapa HP', 'Eqiiperiment'],
     ['3', 'sample.31', 'FooBar_666_p1', 'A5', 'iTru7_107_11', 'CAATGTGG',
-     'iTru5_01_A', 'GGTACGAA', 'FooBar_666', 'pool_1', 'importantsample31',
+     'iTru5_01_A', 'GGTACGAA', 'FooBar_666', 'pool1', 'importantsample31',
      'Knight Lab Kapa HP', 'SomethingWitty'],
     ['3', 'sample.32', 'FooBar_666_p1', 'B6', 'iTru7_107_12', 'AAGGCTGA',
-     'iTru5_01_A', 'CGATCGAT', 'FooBar_666', 'pool_1', 'importantsample32',
+     'iTru5_01_A', 'CGATCGAT', 'FooBar_666', 'pool1', 'importantsample32',
      'Knight Lab Kapa HP', 'SomethingWitty'],
     ['3', 'sample.34', 'FooBar_666_p1', 'B8', 'iTru7_107_13', 'TTACCGAG',
-     'iTru5_01_A', 'AAGACACC', 'FooBar_666', 'pool_1', 'importantsample34',
+     'iTru5_01_A', 'AAGACACC', 'FooBar_666', 'pool1', 'importantsample34',
      'Knight Lab Kapa HP', 'SomethingWitty'],
     ['3', 'sample.44', 'Baz_p3', 'B99', 'iTru7_107_14', 'GTCCTAAG',
-     'iTru5_01_A', 'CATCTGCT', 'Baz', 'pool_1', 'importantsample44',
+     'iTru5_01_A', 'CATCTGCT', 'Baz', 'pool1', 'importantsample44',
      'Knight Lab Kapa HP', 'Eqiiperiment']]
 
 
